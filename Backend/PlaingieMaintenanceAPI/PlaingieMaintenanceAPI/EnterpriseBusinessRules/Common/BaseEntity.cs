c9903b0dd6761f1afb6b9e8f86904f9e0fb8a5d8
angelajanebouw <angelabouw@yahoo.es>
2023-12-25 22:58:12 +0100

    Added base entity

---
+++ Backend/PlaingieMaintenanceAPI/PlaingieMaintenanceAPI/EnterpriseBusinessRules/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace PlaingieMaintenanceAPI.EnterpriseBusinessRules.Common;
+
+public class BaseEntity
+{
+    public int UId { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
