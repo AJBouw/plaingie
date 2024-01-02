1a4d3d7da66c1f7d72b6d201cf7664e1bf14ce38
angelajanebouw <angelabouw@yahoo.es>
2024-01-02 19:34:16 +0100

    Implemented data annotations

--- Backend/PlaingieMaintenanceAPI/PlaingieMaintenanceAPI/EnterpriseBusinessRules/Entities/MicroControllerUnit.cs
+++ Backend/PlaingieMaintenanceAPI/PlaingieMaintenanceAPI/EnterpriseBusinessRules/Entities/MicroControllerUnit.cs
@@ -1,10 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using PlaingieMaintenanceAPI.EnterpriseBusinessRules.Common;
 
 namespace PlaingieMaintenanceAPI.EnterpriseBusinessRules.Entities;
 
 public class MicroControllerUnit : BaseEntity
 {
+    [MaxLength(46)]
+    [MinLength(12)]
     public string IpId { get; set; }
+    
+    
+    [MaxLength(17)]
+    [MinLength(12)]
+    [Required]
     public string MacId { get; set; }
+    
+    [MaxLength(50)]
+    [MinLength(8)]
+    [Required]
     public string MqttClientUId { get; set; }
 }
